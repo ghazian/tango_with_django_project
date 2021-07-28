467c4890cc1378311350374159b56c664707a4c8
Ghazian Azfar <azfar.ghazian@gmail.com>
2021-07-28 21:02:57 +0100

    Chapter 6 Exercises

--- populate_rango.py
+++ populate_rango.py
@@ -8,31 +8,31 @@ from rango.models import Category, Page
 def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
+         'url':'http://docs.python.org/3/tutorial/', 'views':70},
         {'title': 'How to Think like a Computer Scientist',
-         'url': 'http://www.greenteapress.com/thinkpython/'},
+         'url': 'http://www.greenteapress.com/thinkpython/', 'views': 33},
         {'title': 'Learn Python in 10 Minutes',
-         'url': 'http://www.korokithakis.net/tutorials/python/'}
+         'url': 'http://www.korokithakis.net/tutorials/python/', 'views': 3}
     ]
 
     django_pages = [
         {'title': 'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views': 80},
         {'title': 'Django Rocks',
-         'url': 'http://www.djangorocks.com/'},
+         'url': 'http://www.djangorocks.com/', 'views': 51},
         {'title': 'How to Tango with Django',
-         'url': 'http://www.tangowithdjango.com/'}
+         'url': 'http://www.tangowithdjango.com/', 'views': 99}
     ]
 
     other_pages = [
         {'title': 'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+         'url':'http://bottlepy.org/docs/dev/', 'views': 40},
         {'title': 'Flask',
-         'url': 'http://flask.pocoo.org'}
+         'url': 'http://flask.pocoo.org', 'views': 21}
     ]
 
-    cats = {'Python': {'pages': python_pages, 'likes':64, 'views':128},
-            'Django': {'pages': django_pages, 'likes':32, 'views':64 },
+    cats = {'Python': {'pages': python_pages, 'views':64, 'likes':128},
+            'Django': {'pages': django_pages, 'views':32, 'likes':64 },
             'Other Frameworks': {'pages': other_pages, 'likes': 32, 'views':32}
             }
 
--- rango/views.py
+++ rango/views.py
@@ -4,9 +4,11 @@ from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = "Crunchy, creamy, cookie, candy, cupcake!"
+    context_dict['pages'] = page_list
     context_dict['categories'] = category_list
     return render(request, 'rango/index.html', context=context_dict)
 
