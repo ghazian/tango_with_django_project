80915203801647794b172a2f5d370ceffc54fd50
Ghazian Azfar <azfar.ghazian@gmail.com>
2021-07-28 20:45:39 +0100

    Chapter 6.7

--- rango/views.py
+++ rango/views.py
@@ -9,3 +9,16 @@ def index(request):
     context_dict['boldmessage'] = "Crunchy, creamy, cookie, candy, cupcake!"
     context_dict['categories'] = category_list
     return render(request, 'rango/index.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
