6eb3d6a77df007bbd52884f105a78a6fd022bf29
Ghazian Azfar <azfar.ghazian@gmail.com>
2021-07-28 18:16:35 +0100

    Complete Chapter 5 and Chapter 5 Exercises

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # category_list = Category.objects.order_by('name')[:5]
+    # context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context=context_dict)
